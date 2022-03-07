f8a38e70d3633e98d2693c7ba160e3e4df338ed3
BeshoyHani <be4oy.hani@gmail.com>
2022-03-07 13:01:47 +0200

    Client Server App File Transfer

---
+++ Client/Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set IP and Port of Server to communicate with
+            IPAddress host = IPAddress.Parse("127.0.0.1");
+            IPEndPoint hostEndpoint = new IPEndPoint(host, 8000);
+
+            Socket clientSock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            clientSock.Connect(hostEndpoint);
+
+            string serverMessage = "";
+
+            //wait for server to send File
+            byte[] receivedData = new byte[1024];
+            int receivedBytesLen = clientSock.Receive(receivedData);
+            int fileNameLen = BitConverter.ToInt32(receivedData);
+            string fileName = Encoding.ASCII.GetString(receivedData, 4, fileNameLen);
+            serverMessage = Encoding.ASCII.GetString(receivedData, 4 + fileNameLen, receivedBytesLen);
+            // Store Received Data in a file
+            File.WriteAllText("received file.txt", serverMessage);
+            Console.WriteLine("Server Sent: {0}\n{1}", fileName, serverMessage);
+
+            //Close Socket
+            Console.WriteLine("Transfer Ended");
+            clientSock.Shutdown(SocketShutdown.Both);
+            clientSock.Close();
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Set Server IP, Port and Protocol
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(serverIP, 8000);
+            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            sock.Bind(ipEnd);
+            sock.Listen(100);
+            Console.WriteLine("Listening at IP: 127.0.0.1, Port: 8000");
+
+            //Getting File Data ready to be sent
+            string fileName = args.Length>0 && args[0]!="fileName"? args[0] : "test.txt";
+            byte[] fileNameByte = Encoding.ASCII.GetBytes(fileName);
+            byte[] fileNameLength = BitConverter.GetBytes(fileName.Length);
+            byte[] fileContent = File.ReadAllBytes(fileName);
+            int FileLength = 4 + fileNameByte.Length + fileContent.Length;
+            byte[] file = new byte[FileLength];
+
+            fileNameLength.CopyTo(file, 0);
+            fileNameByte.CopyTo(file, 4);
+            fileContent.CopyTo(file, 4 + fileNameByte.Length);
+
+            //Wait for Client
+            Socket clientSock = sock.Accept();
+            Console.WriteLine("Client Accepted");
+
+            //Send Client a message
+
+            Console.WriteLine("Sending File: {0}",fileName);
+            clientSock.Send(file);
+            Console.WriteLine("File Sent");
+            byte[] messageByteArray = new byte[1024];
+            int receivedBytesLen = clientSock.Receive(messageByteArray);
+
+            //Close Socket
+            Console.WriteLine("Transfer Ended");
+            clientSock.Shutdown(SocketShutdown.Both);
+            clientSock.Close();
+
+
+        }
+    }
+}
